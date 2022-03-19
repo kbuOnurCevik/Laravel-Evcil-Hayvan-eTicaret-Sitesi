c428bb67bf847890f564b40dae16fade34d53800
kbuOnurCevik <2017010213001@ogrenci.karabuk.edu.tr>
2022-03-19 15:30:57 +0300

    Product CRUD

--- resources/views/admin/_sidebar.blade.php
+++ resources/views/admin/_sidebar.blade.php
@@ -11,66 +11,66 @@
         </a>
 
     </li>
-    <!-- <li class="nav-item">
-         <a class="nav-link collapsed" href="#">
+     <li class="nav-item">
+         <a class="nav-link collapsed" href="{{route('admin_products')}}">
              <i class="far fa-fw fa-window-maximize"></i>
              <span>Products</span>
          </a>
 
      </li>
-
-     <li class="nav-item">
-         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable" aria-expanded="true"
-            aria-controls="collapseTable">
-             <i class="fas fa-fw fa-table"></i>
-             <span>Orders</span>
-         </a>
-         <div id="collapseTable" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar">
-             <div class="bg-white py-2 collapse-inner rounded">
-                 <h6 class="collapse-header">Orders</h6>
-                 <a class="collapse-item" href="#">All Orders</a>
-                 <a class="collapse-item" href="#">New Orders</a>
-                 <a class="collapse-item" href="#">Canceled Orders</a>
-                 <a class="collapse-item" href="#">Completed Orders</a>
+    <!--
+         <li class="nav-item">
+             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable" aria-expanded="true"
+                aria-controls="collapseTable">
+                 <i class="fas fa-fw fa-table"></i>
+                 <span>Orders</span>
+             </a>
+             <div id="collapseTable" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar">
+                 <div class="bg-white py-2 collapse-inner rounded">
+                     <h6 class="collapse-header">Orders</h6>
+                     <a class="collapse-item" href="#">All Orders</a>
+                     <a class="collapse-item" href="#">New Orders</a>
+                     <a class="collapse-item" href="#">Canceled Orders</a>
+                     <a class="collapse-item" href="#">Completed Orders</a>
+                 </div>
              </div>
-         </div>
-     </li>
+         </li>
 
-     <li class="nav-item">
-         <a class="nav-link collapsed" href="#"
-            aria-controls="collapseForm">
-             <i class="fab fa-fw fa-wpforms"></i>
-             <span>Settings</span>
-         </a>
-     </li>
-     <li class="nav-item">
-         <a class="nav-link collapsed" href="#"
-            aria-controls="collapseForm">
-             <i class="fab fa-fw fa-wpforms"></i>
-             <span>Users</span>
-         </a>
-     </li>
-     <li class="nav-item">
-         <a class="nav-link collapsed" href="#"
-            aria-controls="collapseForm">
-             <i class="fab fa-fw fa-wpforms"></i>
-             <span>Contact Messages</span>
-         </a>
-     </li>
-     <li class="nav-item">
-         <a class="nav-link collapsed" href="#"
-            aria-controls="collapseForm">
-             <i class="fab fa-fw fa-wpforms"></i>
-             <span>Reviews</span>
-         </a>
-     </li>
-     <li class="nav-item">
-         <a class="nav-link collapsed" href="#"
-            aria-controls="collapseForm">
-             <i class="fab fa-fw fa-wpforms"></i>
-             <span>FAQ</span>
-         </a>
-     </li>-->
+         <li class="nav-item">
+             <a class="nav-link collapsed" href="#"
+                aria-controls="collapseForm">
+                 <i class="fab fa-fw fa-wpforms"></i>
+                 <span>Settings</span>
+             </a>
+         </li>
+         <li class="nav-item">
+             <a class="nav-link collapsed" href="#"
+                aria-controls="collapseForm">
+                 <i class="fab fa-fw fa-wpforms"></i>
+                 <span>Users</span>
+             </a>
+         </li>
+         <li class="nav-item">
+             <a class="nav-link collapsed" href="#"
+                aria-controls="collapseForm">
+                 <i class="fab fa-fw fa-wpforms"></i>
+                 <span>Contact Messages</span>
+             </a>
+         </li>
+         <li class="nav-item">
+             <a class="nav-link collapsed" href="#"
+                aria-controls="collapseForm">
+                 <i class="fab fa-fw fa-wpforms"></i>
+                 <span>Reviews</span>
+             </a>
+         </li>
+         <li class="nav-item">
+             <a class="nav-link collapsed" href="#"
+                aria-controls="collapseForm">
+                 <i class="fab fa-fw fa-wpforms"></i>
+                 <span>FAQ</span>
+             </a>
+         </li>-->
 
      <hr class="sidebar-divider">
      <div class="version" id="version-ruangadmin"></div>
--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,44 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::get('/home2', function () {
     return view('welcome');
 });
 
-Route::get('/',[HomeController::class,'index']);
-Route::get('/home',[HomeController::class,'index'])->name('home');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
-Route::middleware('auth')->prefix('admin')->group(function (){
-    Route::get('/',[\App\Http\Controllers\Admin\HomeController::class,'index'])->name('admin_home');
-    Route::get('category',[\App\Http\Controllers\Admin\CategoryController::class,'index'])->name('admin_category');
-    Route::get('category/add',[\App\Http\Controllers\Admin\CategoryController::class,'add'])->name('admin_category_add');
-    Route::post('category/create',[\App\Http\Controllers\Admin\CategoryController::class,'create'])->name('admin_category_create');
-    Route::get('category/edit/{id}',[\App\Http\Controllers\Admin\CategoryController::class,'edit'])->name('admin_category_edit');
-    Route::post('category/update/{id}',[\App\Http\Controllers\Admin\CategoryController::class,'update'])->name('admin_category_update');
-    Route::get('category/delete/{id}',[\App\Http\Controllers\Admin\CategoryController::class,'destroy'])->name('admin_category_delete');
-    Route::get('category/show',[\App\Http\Controllers\Admin\CategoryController::class,'show'])->name('admin_category_show');
+Route::middleware('auth')->prefix('admin')->group(function () {
+
+    Route::get('/', [\App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin_home');
+
+    #Category
+    Route::get('category', [\App\Http\Controllers\Admin\CategoryController::class, 'index'])->name('admin_category');
+    Route::get('category/add', [\App\Http\Controllers\Admin\CategoryController::class, 'add'])->name('admin_category_add');
+    Route::post('category/create', [\App\Http\Controllers\Admin\CategoryController::class, 'create'])->name('admin_category_create');
+    Route::get('category/edit/{id}', [\App\Http\Controllers\Admin\CategoryController::class, 'edit'])->name('admin_category_edit');
+    Route::post('category/update/{id}', [\App\Http\Controllers\Admin\CategoryController::class, 'update'])->name('admin_category_update');
+    Route::get('category/delete/{id}', [\App\Http\Controllers\Admin\CategoryController::class, 'destroy'])->name('admin_category_delete');
+    Route::get('category/show', [\App\Http\Controllers\Admin\CategoryController::class, 'show'])->name('admin_category_show');
+
+    # Product
+    Route::prefix('product')->group(function () {
+
+        Route::get('/', [\App\Http\Controllers\Admin\ProductController::class, 'index'])->name('admin_products');
+        Route::get('create', [\App\Http\Controllers\Admin\ProductController::class, 'create'])->name('admin_product_add');
+        Route::post('store', [\App\Http\Controllers\Admin\ProductController::class, 'store'])->name('admin_product_store');
+        Route::get('edit/{id}', [\App\Http\Controllers\Admin\ProductController::class, 'edit'])->name('admin_product_edit');
+        Route::post('update/{id}', [\App\Http\Controllers\Admin\ProductController::class, 'update'])->name('admin_product_update');
+        Route::get('delete/{id}', [\App\Http\Controllers\Admin\ProductController::class, 'destroy'])->name('admin_product_delete');
+        Route::get('show', [\App\Http\Controllers\Admin\ProductController::class, 'show'])->name('admin_product_show');
+    });
+
 });
 
-Route::get('/admin/login',[\App\Http\Controllers\Admin\HomeController::class,'login'])->name('admin_login');
-Route::post('/admin/logincheck',[\App\Http\Controllers\Admin\HomeController::class,'logincheck'])->name('admin_logincheck');
-Route::get('/admin/logout',[\App\Http\Controllers\Admin\HomeController::class,'logout'])->name('admin_logout');
+Route::get('/admin/login', [\App\Http\Controllers\Admin\HomeController::class, 'login'])->name('admin_login');
+Route::post('/admin/logincheck', [\App\Http\Controllers\Admin\HomeController::class, 'logincheck'])->name('admin_logincheck');
+Route::get('/admin/logout', [\App\Http\Controllers\Admin\HomeController::class, 'logout'])->name('admin_logout');
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
